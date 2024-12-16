036e5fab555ae3911963d437ec4611e6d31ebee9
c-doo95 <116585057+c-doo95@users.noreply.github.com>
2024-12-16 13:13:29 -0500

    create test for win conditions

--- script.js
+++ script.js
@@ -38,6 +38,8 @@ function GameController(playerXName = "Player X", playerOName = "Player O") {
     },
   ];
 
+  const board = gameboard.getBoard();
+
   let activePlayer = players[0];
 
   const switchPlayerTurn = () => {
@@ -54,8 +56,50 @@ function GameController(playerXName = "Player X", playerOName = "Player O") {
   const playRound = (row, column) => {
     gameboard.placeToken(row, column, getActivePlayer().token);
 
-    switchPlayerTurn();
-    printNewRound();
+    if (checkWin() === false) {
+      switchPlayerTurn();
+      printNewRound();
+    }
+  };
+
+  const checkWin = () => {
+    let token = activePlayer.token;
+
+    if (
+      board[0][0] === token &&
+      board[0][0] === board[1][1] &&
+      board[0][0] === board[2][2]
+    ) {
+      console.log(`${activePlayer.name} wins!`);
+      return true;
+    } else if (
+      board[0][2] === token &&
+      board[0][2] === board[1][1] &&
+      board[0][2] === board[2][0]
+    ) {
+      console.log(`${activePlayer.name} wins!`);
+      return true;
+    } else {
+      for (let i = 0; i < 3; i++) {
+        if (
+          board[i][0] === token &&
+          board[i][0] === board[i][1] &&
+          board[i][0] === board[i][2]
+        ) {
+          console.log(`${activePlayer.name} wins!`);
+          return true;
+        } else if (
+          board[0][i] === token &&
+          board[0][i] === board[1][i] &&
+          board[0][i] === board[2][i]
+        ) {
+          console.log(`${activePlayer.name} wins!`);
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
   };
 
   printNewRound();
